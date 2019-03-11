8c3df557cb3e2633ef35a99a1b8753c6a0899a56
Przemyslaw Jaros <przemyslaw.jaros@pwc.com>
2019-03-11 14:45:07 +0100

    Added controller and some views

---
+++ src/Web/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Home";
+            return View();
+        }
+    }
+}

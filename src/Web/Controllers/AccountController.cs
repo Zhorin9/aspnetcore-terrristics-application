c8bb950a900fada18562c704a964cf5948f28dfd
Przemysław Jaros <przemek7799@hotmail.com>
2019-03-19 22:57:58 +0100

    Added account signIn method, preapared viewModel for post action

---
+++ src/Web/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Web.Identity;
+using Web.ViewModels;
+
+namespace Web.Controllers
+{
+    [Authorize]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult SignIn()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+
+
+        [HttpPost]
+        [AllowAnonymous]
+        public Task<IActionResult> SignIn(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = _signInManager.PasswordSignInAsync()
+            }
+
+            ModelState.AddModelError("", "Nie udało się zalogować");
+
+            return View();
+        }
+    }
+}
---
+++ src/Web/ViewModels/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Błędny email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Hasło")]
+        public string Password { get; set; }
+
+        [Display(Name = "Zapamiętać mnie?")]
+        public bool RememberMe { get; set; }
+    }
+}

171ae182593c84da82ab6c2f38d50a12ff059e2c
Przemyslaw Jaros <przemek7799@gmail.com>
2019-03-22 15:16:06 +0100

    Added error message resources for polish lang, Configure culture localization

--- src/Web/Startup.cs
+++ src/Web/Startup.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -43,7 +46,11 @@ namespace TerrristicsApp
             services.AddTransient<AppIdentityDbContextSeed>();
             services.AddTransient<AddDbContextSeed>();
 
-            services.AddMvc();
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+            services.AddMvc()
+                .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
+                .AddDataAnnotationsLocalization();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,6 +63,17 @@ namespace TerrristicsApp
 
             app.UseStaticFiles();
             app.UseNodeModules(env);
+
+            var supportedCultures = new[]
+            {
+                new CultureInfo("pl-PL"),
+            };
+            app.UseRequestLocalization(new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture("pl-PL"),
+                SupportedCultures = supportedCultures,
+                SupportedUICultures = supportedCultures
+            });
 
             app.UseAuthentication();
 

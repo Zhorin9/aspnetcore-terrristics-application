a16459f1ef7155703dc755459ab5adfdddd0d769
Przemyslaw Jaros <przemek7799@gmail.com>
2019-03-27 15:51:08 +0100

    Added async repository

---
+++ src/AppCore/Interfaces/IAsyncAppRepository.cs
@@ -0,0 +1,52 @@
+﻿using AppCore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace AppCore.Interfaces
+{
+    public interface IAsyncAppRepository<T> where T : BaseEntity
+    {
+        /// <summary>
+        /// Get entity based on id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<T> GetByIdAsync(int id);
+
+        /// <summary>
+        /// Get listed data for specified entity
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<T>> ListAllAsync();
+
+        /// <summary>
+        /// Add data
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        void AddAsync(T entity);
+
+        /// <summary>
+        /// Update data
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        void UpdateAsync(T entity);
+
+        /// <summary>
+        /// Delete data
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        void DeleteAsync(T entity);
+
+        /// <summary>
+        /// Get list of data by expression
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <returns></returns>
+        Task<IEnumerable<T>> GetListByExpression(Expression<Func<T, bool>> expression);
+    }
+}
---
+++ src/Infrastructure/Data/AppRepository.cs
@@ -0,0 +1,53 @@
+﻿using AppCore.Entities;
+using AppCore.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class AppRepository<T> : IAsyncAppRepository<T> where T : BaseEntity
+    {
+        private readonly AppDbContext _context;
+
+        public AppRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async void AddAsync(T entity)
+        {
+            _context.Set<T>().Add(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async void UpdateAsync(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async void DeleteAsync(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<T>> ListAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetListByExpression(Expression<Func<T, bool>> expression)
+        {
+            return await _context.Set<T>().Where(expression).ToListAsync();
+        }
+    }
+}

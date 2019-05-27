93874efca233a16b22c3bf4d63747eed2f2c038b
Przemysław Jaros <przemek7799@gmail.com>
2019-05-27 22:04:48 +0200

    Added axios, started creating services and store

--- src/Web/client-app/src/main.js
+++ src/Web/client-app/src/main.js
@@ -2,14 +2,22 @@ import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
 import App from './App.vue';
 import router from './router';
+import store from './store';
+import axios from 'axios';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
+//Install Vue bootstrap extension
 Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
+
+// Setup axios as the Vue default $http library
+axios.defaults.baseURL = 'http://localhost:5000'
+Vue.prototype.$http = axios
 
 new Vue({
   render: h => h(App),
   router,
+  store,
 }).$mount('#app');
---
+++ src/Web/client-app/src/service/user.service.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+// const client = axios.create({
+//     baseURL: 'http://localhost:'
+// })
+
+export const userService = {
+    login, 
+    logout,
+    register,
+};
+
+function login(username, password){
+    axios.post()
+}

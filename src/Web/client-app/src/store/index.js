6764228bf2234b27601e85f534dd8c5840d3fdd2
Przemysław Jaros <przemek7799@gmail.com>
2019-06-23 21:52:13 +0200

    Added alert module

--- src/Web/client-app/src/store/index.js
+++ src/Web/client-app/src/store/index.js
@@ -3,12 +3,14 @@ import Vuex from 'vuex';
 
 import { authentication } from './authentication.module';
 import { users } from './users.module';
+import { alert } from './alert.module';
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
     modules: {
         authentication,
-        users
+        users,
+        alert
     }
 });

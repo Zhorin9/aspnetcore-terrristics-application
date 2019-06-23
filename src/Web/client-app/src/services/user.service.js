7452610dde7f267df2a42b58822e1ce6d14dfb4a
Przemysław Jaros <przemek7799@gmail.com>
2019-06-23 21:57:25 +0200

    Fixed user service to return data

--- src/Web/client-app/src/services/user.service.js
+++ src/Web/client-app/src/services/user.service.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import { authHeader } from '../helpers/auth.header.js';
 
 export const userService = {
     login, 
@@ -12,38 +11,29 @@ function login(email, password){
         'Access-Control-Allow-Origin': '*',
         'Content-Type': 'application/json',    
     };
-    axios.post('/account/createToken',
-    { Email: email, Password: password }, 
-    { headers: headers })
-    .then(handleResponse)
-    .then(user => {
-        if(user.token){
-            localStorage.setItem('user', JSON.stringify(user));
-        }
-
-        return user;
-    });
+    return axios.post('/account/createToken',
+            { Email: email, Password: password }, 
+            { headers: headers })
+        .then(data => {
+            if(data.token){
+                var user = { Email: email, Token: data.token, TokenExpiration: data.expiration}
+                localStorage.setItem('user', JSON.stringify(user));
+            }
+            return user;
+        })
+        .catch(handleError);
 }
 
 //Remove user from local storage to log user out
 function logout() {
     localStorage.removeItem('user');
 }
-
-function handleResponse(response){
-    return response.text().then(text => {
-        const data = text && JSON.parse(text);
-        if (!response.ok) {
-            if (response.status === 401) {
-                // auto logout if 401 response returned from api
-                logout();
-                location.reload(true);
-            }
-
-            const error = (data && data.message) || response.statusText;
-            return Promise.reject(error);
-        }
-
-        return data;
-    });
+function handleError(data){
+    if(data.response.status === 401){
+        // auto logout if 401 response returned from api
+        logout();
+        location.reload(true);
+    }
+    const error = (data.response.data.message) || data.response.status;
+    return Promise.reject(error);
 }
